cb38e4548a853b5a0e2b7abd59aba12ed43fbae1
sebastien jouhans <sebastien.jouhans@gmail.com>
2019-01-12 23:45:52 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# rotary-encoder-to-mouse-ouput
-Arduino project mapping rotary encoder to mouse output
+## Overview
+
+This arduino code takes the rotation of the rotary encoder output and turns them into mouse ouputs. The rotation is mapped the mouse wheel and the push button is mapped to a left button click.
+
+This makes the data coming from the endocer is to consum by any apps at it is just a mouse input
+
+## Equipment
+
+|Component|Cost|Description|Link|
+|:-:|:-:|:-:|:-:|
+| Arduino Leonardo | £ 15  | Micro controller |[RS](https://uk.rs-online.com/web/p/processor-microcontroller-development-kits/7617324/) |
+| Bourns Rotary Encoder w/ push button | £13  | part: EM14R0D-R20-L032S |[RS](https://uk.rs-online.com/web/p/rotary-encoders/8274997/) |
+| Bourns cable with connector | £5  | part: H-290-4 |[Mouser](https://www.mouser.co.uk/ProductDetail/Bourns/H-290-4?qs=oa0Mu2YftbVpRz7K7Ip3%2FA%3D%3D) |
+
+## Wiring
+
+This is wiring digram that i used for this sample.
+
+![Alt text](assets/Wiring_diagram.png?raw=true "Wiring diagram")
---
+++ arduino/rotary-encoder-to-mouse/rotary-encoder-to-mouse.ino
@@ -0,0 +1,134 @@
+#include <Mouse.h>
+
+#define encoder0PinA  2
+#define encoder0PinB  3
+#define pushButtonPin  4
+#define maxEncoderValue  1024
+
+int currentPushButtonState = 0;
+int previousPushButtonState = 0;
+
+float scrub = 0;
+float encoderTick = 0;
+
+volatile unsigned int currentEncoder0Pos = 0;
+volatile unsigned int previouscurrentEncoder0Pos = 0;
+
+void setup() {
+  pinMode(encoder0PinA, INPUT);
+  pinMode(encoder0PinB, INPUT);
+  pinMode(pushButtonPin, INPUT_PULLUP);
+
+  // encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+
+  // encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE);
+
+  Mouse.begin();
+
+  Serial.begin (9600);
+}
+
+void loop() {
+  
+  updateButtonState();
+
+  updateMouseWheel();
+  
+}
+
+void updateEncoderTick() {
+  if (currentEncoder0Pos < 0) {
+    scrub = 1 + (float(currentEncoder0Pos % maxEncoderValue) / float (maxEncoderValue)) ;
+  }
+  else {
+    scrub = float(currentEncoder0Pos % maxEncoderValue) / float (maxEncoderValue);
+  }
+  encoderTick = int((maxEncoderValue * scrub));
+  
+  Serial.println (scrub);
+  Serial.println (encoderTick);
+  Serial.println ("---");
+}
+
+void updateMouseWheel()
+{
+  if(previouscurrentEncoder0Pos != currentEncoder0Pos)
+  {
+    previouscurrentEncoder0Pos = currentEncoder0Pos;
+    //updateEncoderTick();
+    Mouse.move(0,0,currentEncoder0Pos);
+  }
+}
+
+void updateButtonState()
+{
+  currentPushButtonState = digitalRead(pushButtonPin);
+  if(currentPushButtonState != previousPushButtonState)
+  {
+    previousPushButtonState = currentPushButtonState;
+    Serial.println (currentPushButtonState, DEC);
+    if (currentPushButtonState == HIGH) {
+      Mouse.press();
+    } else {
+      Mouse.release();
+    }
+  }
+}
+
+void doEncoderA() {
+  // look for a low-to-high on channel A
+  if (digitalRead(encoder0PinA) == HIGH) {
+
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinB) == LOW) {
+      currentEncoder0Pos = currentEncoder0Pos + 1;         // CW
+    }
+    else {
+      currentEncoder0Pos = currentEncoder0Pos - 1;         // CCW
+    }
+  }
+
+  else   // must be a high-to-low edge on channel A
+  {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinB) == HIGH) {
+      currentEncoder0Pos = currentEncoder0Pos + 1;          // CW
+    }
+    else {
+      currentEncoder0Pos = currentEncoder0Pos - 1;          // CCW
+    }
+  }
+  Serial.println (currentEncoder0Pos, DEC);
+  // use for debugging - remember to comment out
+}
+
+void doEncoderB() {
+  // look for a low-to-high on channel B
+  if (digitalRead(encoder0PinB) == HIGH) {
+
+    // check channel A to see which way encoder is turning
+    if (digitalRead(encoder0PinA) == HIGH) {
+      currentEncoder0Pos = currentEncoder0Pos + 1;         // CW
+    }
+    else {
+      currentEncoder0Pos = currentEncoder0Pos - 1;         // CCW
+    }
+  }
+
+  // Look for a high-to-low on channel B
+
+  else {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinA) == LOW) {
+      currentEncoder0Pos = currentEncoder0Pos + 1;          // CW
+    }
+    else {
+      currentEncoder0Pos = currentEncoder0Pos - 1;          // CCW
+    }
+  }
+}
+
+
+

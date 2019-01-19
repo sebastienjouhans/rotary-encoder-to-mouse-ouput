ce82265f8a087318f256c4b5f3cc0d90ffaa4452
Sebastien Jouhans <sebastien.jouhans@gmail.com>
2019-01-19 16:23:31 +0000

    replace mouse click with keyboard input which allowed for a new one

--- arduino/rotary-encoder-to-mouse/rotary-encoder-to-mouse.ino
+++ arduino/rotary-encoder-to-mouse/rotary-encoder-to-mouse.ino
@@ -1,12 +1,20 @@
 #include <Mouse.h>
+#include <Keyboard.h>
 
 #define encoder0PinA  2
 #define encoder0PinB  3
-#define pushButtonPin  4
+#define pushButton1Pin  4
+#define pushButton2Pin  5
 #define maxEncoderValue  1024
 
-int currentPushButtonState = 0;
-int previousPushButtonState = 0;
+char keyA = 'a';
+char keyB = 'b';
+
+int currentPushButton1State = 0;
+int previousPushButton1State = 0;
+
+int currentPushButton2State = 0;
+int previousPushButton2State = 0;
 
 float scrub = 0;
 float encoderTick = 0;
@@ -21,7 +29,9 @@ bool isWheelValuePositive = true;
 void setup() {
   pinMode(encoder0PinA, INPUT);
   pinMode(encoder0PinB, INPUT);
-  pinMode(pushButtonPin, INPUT_PULLUP);
+
+  pinMode(pushButton1Pin, INPUT_PULLUP);
+  pinMode(pushButton2Pin, INPUT_PULLUP);
 
   // encoder pin on interrupt 0 (pin 2)
   attachInterrupt(0, doEncoderA, CHANGE);
@@ -30,13 +40,15 @@ void setup() {
   attachInterrupt(1, doEncoderB, CHANGE);
 
   Mouse.begin();
+  Keyboard.begin();
 
   Serial.begin (9600);
 }
 
 void loop() {
   
-  updateButtonState();
+  updateButton1State();
+  updateButton2State();
 
   updateMouseWheel();
   
@@ -56,6 +68,8 @@ void loop() {
 //  Serial.println ("---");
 //}
 
+// output mousewheel positive value when going in one direction
+// and output a negative value when going in the opposite direction
 void updateMouseWheel()
 {
   if(previouscurrentEncoder0Pos != currentEncoder0Pos)
@@ -80,22 +94,39 @@ void updateMouseWheel()
     }
     
     previouscurrentEncoder0Pos = currentEncoder0Pos;
-  Serial.println (wheelValue);
+    Serial.println (wheelValue);
     Mouse.move(0,0,wheelValue);
   }
 }
 
-void updateButtonState()
+//ouput keyboard letter a when the button is pressed
+void updateButton1State()
 {
-  currentPushButtonState = digitalRead(pushButtonPin);
-  if(currentPushButtonState != previousPushButtonState)
+  currentPushButton1State = digitalRead(pushButton1Pin);
+  if(currentPushButton1State != previousPushButton1State)
   {
-    previousPushButtonState = currentPushButtonState;
-    Serial.println (currentPushButtonState, DEC);
-    if (currentPushButtonState == HIGH) {
-      Mouse.press();
+    previousPushButton1State = currentPushButton1State;
+    Serial.println (currentPushButton1State, DEC);
+    if (currentPushButton1State == HIGH) {
+      Keyboard.press(keyA);
     } else {
-      Mouse.release();
+      Keyboard.release(keyA);
+    }
+  }
+}
+
+//ouput keyboard letter b when the button is pressed
+void updateButton2State()
+{
+  currentPushButton2State = digitalRead(pushButton2Pin);
+  if(currentPushButton2State != previousPushButton2State)
+  {
+    previousPushButton2State = currentPushButton2State;
+    Serial.println (currentPushButton2State, DEC);
+    if (currentPushButton2State == HIGH) {
+      Keyboard.press(keyB);
+    } else {
+      Keyboard.release(keyB);
     }
   }
 }
